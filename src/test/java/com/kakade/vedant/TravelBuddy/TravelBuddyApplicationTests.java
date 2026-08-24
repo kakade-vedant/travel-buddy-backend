be56745795b62f46433c483086e9a077bb13c0be
Vedant Kakade <kakadevedant13@gmail.com>
2026-08-24 21:58:45 +0530

    Backend Initialization

---
+++ src/test/java/com/kakade/vedant/TravelBuddy/TravelBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakade.vedant.TravelBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
