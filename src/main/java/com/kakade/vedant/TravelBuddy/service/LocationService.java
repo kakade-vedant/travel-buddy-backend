40caffbb8935fec885fa1508e32bfeceb83a3166
Vedant Kakade <kakadevedant13@gmail.com>
2026-08-27 21:27:40 +0530

    Added the Location Service.

--- src/main/java/com/kakade/vedant/TravelBuddy/service/LocationService.java
+++ src/main/java/com/kakade/vedant/TravelBuddy/service/LocationService.java
@@ -1,11 +1,14 @@
 package com.kakade.vedant.TravelBuddy.service;
 
 import com.kakade.vedant.TravelBuddy.exception.IdModificationException;
+import com.kakade.vedant.TravelBuddy.exception.ItemNotFoundException;
 import com.kakade.vedant.TravelBuddy.models.DBEntity.LocationEntity;
 import com.kakade.vedant.TravelBuddy.models.RequestEntity.LocationRequest;
 import com.kakade.vedant.TravelBuddy.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocationService {
@@ -25,5 +28,23 @@ public class LocationService {
         repository.save(locationEntity);
 
         return locationEntity;
+    }
+
+    public List<LocationEntity> getAllLocation() {
+        return repository.findAll();
+    }
+
+    public LocationEntity getLocation(String id) throws ItemNotFoundException {
+        LocationEntity location = findLocation(id);
+
+        if (location == null) {
+            throw new ItemNotFoundException();
+        }
+
+        return location;
+    }
+
+    public LocationEntity findLocation(String id) {
+        return repository.findById(id).orElse(null);
     }
 }
