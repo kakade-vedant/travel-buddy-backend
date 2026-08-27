7944a4db3534c4e6b5bac2c1e8c99cfd9fcc164a
Vedant Kakade <kakadevedant13@gmail.com>
2026-08-27 21:17:22 +0530

    Added missing File.

--- src/main/java/com/kakade/vedant/TravelBuddy/exception/ItemNotFoundException.java
+++ src/main/java/com/kakade/vedant/TravelBuddy/exception/ItemNotFoundException.java
@@ -1,2 +1,11 @@
-package com.kakade.vedant.TravelBuddy.exception;public class NotFoundException {
+package com.kakade.vedant.TravelBuddy.exception;
+
+public class ItemNotFoundException extends Exception{
+    public ItemNotFoundException() {
+        super("Item Not Found.");
+    }
+
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
 }
